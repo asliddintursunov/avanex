78d00dd41aa6cca81a25ee4c63cb44970572e242
asliddintursunov <asliddintursunovfarkhodovich@gmail.com>
2025-02-26 20:57:27 +0500

    Returned sales GET api is done

---
+++ src/components/Modals/ReturnedSales/ReturnedSalesModal.tsx
@@ -0,0 +1,158 @@
+import {
+  Box,
+  Flex,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Text,
+  Thead,
+  Tr,
+  Th,
+} from "@chakra-ui/react";
+import { ReturnedSalesType } from "../../../types/returned-sales";
+import ParentModal from "../ParentModal";
+import { useTranslation } from "react-i18next";
+
+type Props = {
+  isOpen: boolean;
+  onClose: () => void;
+  returnedSaleItem?: ReturnedSalesType;
+};
+
+function ReturnedSalesModal({ isOpen, onClose, returnedSaleItem }: Props) {
+  const { t } = useTranslation();
+  const tableInfo: {
+    label: string;
+    value: string | number | undefined | null;
+  }[][] = [
+    [
+      {
+        label: t("labels.doc_num"),
+        value: `№ ${returnedSaleItem?.docNum}`,
+      },
+      {
+        label: t("labels.card_name"),
+        value: returnedSaleItem?.cardName,
+      },
+      {
+        label: t("labels.doc_date"),
+        value: returnedSaleItem?.docDate.split("T")[0],
+      },
+      {
+        label: t("labels.doc_total"),
+        value: `${returnedSaleItem?.docTotalSys} USD`,
+      },
+      {
+        label: t("labels.doc_total"),
+        value: `${returnedSaleItem?.docTotalFc} UZS`,
+      },
+      {
+        label: t("labels.paid_to_date"),
+        value: returnedSaleItem?.paidToDate,
+      },
+    ],
+    [
+      {
+        label: t("labels.paid_sum"),
+        value: returnedSaleItem?.paidSum,
+      },
+      {
+        label: t("labels.u_type_order"),
+        value: returnedSaleItem?.uTypeOrder || "Null",
+      },
+      {
+        label: t("labels.u_tip_dostavka"),
+        value: returnedSaleItem?.uTipdostavka || "Null",
+      },
+      {
+        label: t("labels.u_jonatildi"),
+        value: returnedSaleItem?.uJonatildi || "Null",
+      },
+      { label: t("labels.u_yopildi"), value: returnedSaleItem?.uYopildi },
+      { label: t("labels.u_firma"), value: returnedSaleItem?.uFirma || "Null" },
+    ],
+  ];
+
+  const ReturnedSaleItemTable = (): JSX.Element => {
+    return (
+      <Table variant="striped" colorScheme="gray" textAlign="center">
+        <Thead>
+          <Tr>
+            <Th>{t("labels.quantity")}</Th>
+            <Th>{t("labels.price")}</Th>
+            <Th>{t("labels.price")}</Th>
+            <Th>{t("labels.line_total")}</Th>
+            <Th>{t("labels.row_total")}</Th>
+            <Th>{t("labels.item_code")}</Th>
+            <Th>{t("labels.item_name")}</Th>
+            <Th>{t("labels.weight")}</Th>
+            <Th>{t("labels.u_rulon")}</Th>
+            <Th>{t("labels.u_list")}</Th>
+            <Th>{t("labels.u_kesilgan_soni")}</Th>
+            <Th>{t("labels.warehouse_code")}</Th>
+            <Th>{t("labels.warehouse_name")}</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {returnedSaleItem?.documentLines.length &&
+            returnedSaleItem.documentLines.map((item, index) => (
+              <Tr key={index}>
+                <Td>{item.quantity}</Td>
+                <Td>{item.priceSC} USD</Td>
+                <Td>{item.priceFC} UZS</Td>
+                <Td>{item.lineTotal}</Td>
+                <Td>{item.rowTotalFC} UZS</Td>
+                <Td>{item.itemCode}</Td>
+                <Td>{item.itemDescription}</Td>
+                <Td>{item.weight1}</Td>
+                <Td>{item.uRulon || "Null"}</Td>
+                <Td>{item.uList || "Null"}</Td>
+                <Td>{item.uKesilgansoni || "Null"}</Td>
+                <Td>{item.warehouseCode}</Td>
+                <Td>{item.warehouseName}</Td>
+              </Tr>
+            ))}
+        </Tbody>
+      </Table>
+    );
+  };
+
+  const ModalBody = (): JSX.Element => {
+    return (
+      <TableContainer mb={6}>
+        <Table variant="simple">
+          {[...Array(tableInfo.length)].map((_, nth) => (
+            <Tr key={nth}>
+              {tableInfo[nth].map((info, index) => (
+                <Td key={index}>
+                  <Flex align="center" gap={2}>
+                    <Box>
+                      <Text fontSize="sm" color="gray.500">
+                        {info.label}
+                      </Text>
+                      <Text>{info.value}</Text>
+                    </Box>
+                  </Flex>
+                </Td>
+              ))}
+            </Tr>
+          ))}
+        </Table>
+        <ReturnedSaleItemTable />
+      </TableContainer>
+    );
+  };
+
+  return (
+    <ParentModal
+      body={<ModalBody />}
+      headerContent=""
+      isOpen={isOpen}
+      onClose={onClose}
+      size="6xl"
+    />
+  );
+}
+
+export default ReturnedSalesModal;
--- src/pages/ReturnedSales/index.tsx
+++ src/pages/ReturnedSales/index.tsx
@@ -1,8 +1,151 @@
+import {
+  Box,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  useColorMode,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { useDebounce } from "use-debounce";
+import { ReturnedSalesType } from "../../types/returned-sales";
+import { useRQFetchData } from "../../hooks/useRQfetch";
+import { generateUrlWithParams } from "../../lib/helpers";
+import { useTranslation } from "react-i18next";
+import DateInput from "../../components/Input/DateInput";
+import TextInput from "../../components/Input/TextInput";
+import Pagination from "../../components/Pagination/Pagination";
+import SalesPerons from "../../components/Select/SalesPersonsSelect";
+import TableNoData from "../../components/Table/TableNoData";
+import TableSkeleton from "../../components/Skeleton/TableSkeleton";
+import ReturnedSalesModal from "../../components/Modals/ReturnedSales/ReturnedSalesModal";
+
 function ReturnedSales() {
+  const { colorMode } = useColorMode();
+  const { t } = useTranslation();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const [startDate, setStartDate] = useState<string>("");
+  const [endDate, setEndDate] = useState<string>("");
+  const [cardName, setCardName] = useState<string>("");
+  const [salesPersonName, setSalesPersonName] = useState<string>("");
+  const [skip, setSkip] = useState<number>(0);
+
+  const [debouncedCardName] = useDebounce(cardName, 500);
+  const [returnedSaleItem, setReturnedSaleItem] = useState<ReturnedSalesType>();
+
+  const { data: ReturnedSales, isLoading } = useRQFetchData<{
+    data: ReturnedSalesType[];
+  }>(
+    [
+      `credit-notes-${startDate}-${endDate}-${debouncedCardName}-${salesPersonName}-${skip}`,
+    ],
+    generateUrlWithParams("/credit-notes", {
+      docDateStart: startDate && endDate ? startDate : undefined,
+      docDateEnd: startDate && endDate ? endDate : undefined,
+      cardName: debouncedCardName,
+      salesPersonName: salesPersonName,
+      skip: skip,
+    })
+  );
   return (
-    <div>
-      <h1 className="text-4xl">ReturnedSales</h1>
-    </div>
+    <Box as="div" p={2}>
+      <Box
+        overflowX="auto"
+        boxShadow="base"
+        borderRadius="md"
+        p={4}
+        bg={colorMode === "light" ? "white" : "gray.600"}
+      >
+        <Box
+          as="div"
+          display={"flex"}
+          flexDirection={"row"}
+          alignItems={"end"}
+          gap={"6"}
+          mb="4"
+        >
+          <Box
+            w="full"
+            display="flex"
+            width="50%"
+            alignItems="end"
+            justifyContent="start"
+            gap="4"
+          >
+            <DateInput
+              value={startDate}
+              setValue={setStartDate}
+              label={t("labels.start_date")}
+            />
+            <DateInput
+              value={endDate}
+              setValue={setEndDate}
+              label={t("labels.end_date")}
+            />
+            <TextInput
+              label={t("labels.card_name")}
+              value={cardName}
+              setValue={setCardName}
+            />
+            <SalesPerons setValue={setSalesPersonName} />
+          </Box>
+          <Pagination
+            dataLength={ReturnedSales?.data.length || 0}
+            skip={skip}
+            setSkip={setSkip}
+          />
+        </Box>
+        {!isLoading && (
+          <Table variant="striped" colorScheme="gray" textAlign="center">
+            <Thead bg={colorMode === "light" ? "gray.300" : "gray.800"}>
+              <Tr>
+                <Th>{t("labels.doc_num")}</Th>
+                <Th>{t("labels.card_name")}</Th>
+                <Th>{t("labels.doc_date")}</Th>
+                <Th>{t("labels.doc_total")}</Th>
+                <Th>{t("labels.u_type_order")}</Th>
+                <Th>{t("labels.u_firma")}</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {ReturnedSales?.data.length
+                ? ReturnedSales.data.map((el, idx) => (
+                    <Tr
+                      key={idx}
+                      cursor={"pointer"}
+                      _hover={{
+                        bg: colorMode === "light" ? "gray.300" : "gray.500",
+                      }}
+                      onClick={() => {
+                        setReturnedSaleItem(el);
+                        onOpen();
+                      }}
+                    >
+                      <Td>№{el.docNum}</Td>
+                      <Td>{el.cardName}</Td>
+                      <Td>{el.docDate.split("T")[0]}</Td>
+                      <Td>{`${el.docTotalFc} UZS`}</Td>
+                      <Td>{el.uTypeOrder || "Null"}</Td>
+                      <Td>{el.uFirma || "Null"}</Td>
+                    </Tr>
+                  ))
+                : null}
+            </Tbody>
+          </Table>
+        )}
+        {!ReturnedSales?.data.length && !isLoading && <TableNoData />}
+        {isLoading && <TableSkeleton />}
+      </Box>
+      <ReturnedSalesModal
+        isOpen={isOpen}
+        onClose={onClose}
+        returnedSaleItem={returnedSaleItem}
+      />
+    </Box>
   );
 }
 
